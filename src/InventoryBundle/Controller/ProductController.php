c08c249e5d5a3de480657ae8da2219e2a3c85b02
Ralph <ralphtamayo@creatinginfo.com>
2018-02-21 14:20:05 +0800

    Fixed routings and product listing page

--- src/InventoryBundle/Controller/ProductController.php
+++ src/InventoryBundle/Controller/ProductController.php
@@ -5,7 +5,9 @@ namespace InventoryBundle\Controller;
 use InventoryBundle\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 /**
@@ -15,123 +17,122 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
  */
 class ProductController extends Controller
 {
-    /**
-     * Lists all product entities.
-     *
-     * @Route("/", name="product_index")
-     * @Method("GET")
-     */
-    public function indexAction()
-    {
-        $em = $this->getDoctrine()->getManager();
+	/**
+	 * @Route("/", name="product_index")
+	 * @Template("@Inventory/product/index.html.twig")
+	 * @Method("GET")
+	 */
+	public function indexAction()
+	{
+		$em = $this->getDoctrine()->getManager();
 
-        $products = $em->getRepository('InventoryBundle:Product')->findAll();
+		$products = $em->getRepository('InventoryBundle:Product')->findAll();
 
-        return $this->render('product/index.html.twig', array(
-            'products' => $products,
-        ));
-    }
+		return array(
+			'products' => $products,
+		);
+	}
 
-    /**
-     * Creates a new product entity.
-     * @Security("has_role('ROLE_ADMIN')")
-     * @Route("/new", name="product_new")
-     * @Method({"GET", "POST"})
-     */
-    public function newAction(Request $request)
-    {
-        $product = new Product();
-        $form = $this->createForm('InventoryBundle\Form\ProductType', $product);
-        $form->handleRequest($request);
+	/**
+	 * @Route("/new", name="product_new")
+	 * @Template("@Inventory/product/new.html.twig")
+	 * @Method({"GET", "POST"})
+	 * @Security("has_role('ROLE_ADMIN')")
+	 */
+	public function newAction(Request $request)
+	{
+		$product = new Product();
+		$form = $this->createForm('InventoryBundle\Form\ProductType', $product);
+		$form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($product);
-            $em->flush();
+		if ($form->isSubmitted() && $form->isValid()) {
+			$em = $this->getDoctrine()->getManager();
+			$em->persist($product);
+			$em->flush();
 
-            return $this->redirectToRoute('product_show', array('id' => $product->getId()));
-        }
+			return $this->redirectToRoute('product_show', array('id' => $product->getId()));
+		}
 
-        return $this->render('product/new.html.twig', array(
-            'product' => $product,
-            'form' => $form->createView(),
-        ));
-    }
+		return array(
+			'product' => $product,
+			'form' => $form->createView(),
+		);
+	}
 
-    /**
-     * Finds and displays a product entity.
-     * @Security("has_role('ROLE_ADMIN')")
-     * @Route("/{id}", name="product_show")
-     * @Method("GET")
-     */
-    public function showAction(Product $product)
-    {
-        $deleteForm = $this->createDeleteForm($product);
+	/**
+	 * Finds and displays a product entity.
+	 * @Security("has_role('ROLE_ADMIN')")
+	 * @Route("/{id}", name="product_show")
+	 * @Method("GET")
+	 */
+	public function showAction(Product $product)
+	{
+		$deleteForm = $this->createDeleteForm($product);
 
-        return $this->render('product/show.html.twig', array(
-            'product' => $product,
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
+		return $this->render('product/show.html.twig', array(
+			'product' => $product,
+			'delete_form' => $deleteForm->createView(),
+		));
+	}
 
-    /**
-     * Displays a form to edit an existing product entity.
-     * @Security("has_role('ROLE_ADMIN')")
-     * @Route("/{id}/edit", name="product_edit")
-     * @Method({"GET", "POST"})
-     */
-    public function editAction(Request $request, Product $product)
-    {
-        $deleteForm = $this->createDeleteForm($product);
-        $editForm = $this->createForm('InventoryBundle\Form\ProductType', $product);
-        $editForm->handleRequest($request);
+	/**
+	 * Displays a form to edit an existing product entity.
+	 * @Security("has_role('ROLE_ADMIN')")
+	 * @Route("/{id}/edit", name="product_edit")
+	 * @Method({"GET", "POST"})
+	 */
+	public function editAction(Request $request, Product $product)
+	{
+		$deleteForm = $this->createDeleteForm($product);
+		$editForm = $this->createForm('InventoryBundle\Form\ProductType', $product);
+		$editForm->handleRequest($request);
 
-        if ($editForm->isSubmitted() && $editForm->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
+		if ($editForm->isSubmitted() && $editForm->isValid()) {
+			$this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute('product_show', array('id' => $product->getId()));
-        }
+			return $this->redirectToRoute('product_show', array('id' => $product->getId()));
+		}
 
-        return $this->render('product/edit.html.twig', array(
-            'product' => $product,
-            'edit_form' => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
+		return $this->render('product/edit.html.twig', array(
+			'product' => $product,
+			'edit_form' => $editForm->createView(),
+			'delete_form' => $deleteForm->createView(),
+		));
+	}
 
-    /**
-     * Deletes a product entity.
-     * @Security("has_role('ROLE_ADMIN')")
-     * @Route("/{id}", name="product_delete")
-     * @Method("DELETE")
-     */
-    public function deleteAction(Request $request, Product $product)
-    {
-        $form = $this->createDeleteForm($product);
-        $form->handleRequest($request);
+	/**
+	 * Deletes a product entity.
+	 * @Security("has_role('ROLE_ADMIN')")
+	 * @Route("/{id}", name="product_delete")
+	 * @Method("DELETE")
+	 */
+	public function deleteAction(Request $request, Product $product)
+	{
+		$form = $this->createDeleteForm($product);
+		$form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($product);
-            $em->flush();
-        }
+		if ($form->isSubmitted() && $form->isValid()) {
+			$em = $this->getDoctrine()->getManager();
+			$em->remove($product);
+			$em->flush();
+		}
 
-        return $this->redirectToRoute('product_index');
-    }
+		return $this->redirectToRoute('product_index');
+	}
 
-    /**
-     * Creates a form to delete a product entity.
-     *
-     * @param Product $product The product entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm(Product $product)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('product_delete', array('id' => $product->getId())))
-            ->setMethod('DELETE')
-            ->getForm()
-        ;
-    }
+	/**
+	 * Creates a form to delete a product entity.
+	 *
+	 * @param Product $product The product entity
+	 *
+	 * @return \Symfony\Component\Form\Form The form
+	 */
+	private function createDeleteForm(Product $product)
+	{
+		return $this->createFormBuilder()
+			->setAction($this->generateUrl('product_delete', array('id' => $product->getId())))
+			->setMethod('DELETE')
+			->getForm()
+		;
+	}
 }
