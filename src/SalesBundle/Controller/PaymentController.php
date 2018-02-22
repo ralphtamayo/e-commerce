b196113acad070a0d0eecd499e2df3a7164cc160
Ralph <ralphtamayo@creatinginfo.com>
2018-02-22 14:22:20 +0800

    Finished new payment design

--- src/SalesBundle/Controller/PaymentController.php
+++ src/SalesBundle/Controller/PaymentController.php
@@ -20,12 +20,55 @@ use Symfony\Component\Config\Definition\Exception\Exception;
  */
 class PaymentController extends Controller
 {
+
+	/**
+	 * @Route("/{id}", name="payment_show")
+	 * @Template("@Sales/payment/show.html.twig")
+	 * @Method("GET")
+	 * @Security("has_role('ROLE_USER')")
+	 */
+	public function showAction($id)
+	{
+		$em = $this->getDoctrine()->getManager();
+		$payment = $em->getRepository('SalesBundle:Payment')->find($id);
+
+		return array(
+			'payment' => $payment,
+		);
+	}
+
     /**
 	 * @Route("/{id}/new", name="payment_new")
 	 * @Template("@Sales/payment/new.html.twig")
-	 * @Method({"GET", "POST"})
+	 * @Method("GET")
 	 */
-	public function newAction(Request $request, $id)
+	public function newAction($id)
+	{
+        $payment = new Payment();
+		$form = $this->createForm('SalesBundle\Form\PaymentType', $payment);
+
+		$em = $this->getDoctrine()->getManager();
+
+		$cart = $em->getRepository('SalesBundle:Cart')->find($id);
+		$total = 0;
+
+		foreach($cart->getItems() as $item) {
+			$total += ($item->getQuantity() * $item->getProduct()->getPrice());
+		}
+
+		return array(
+			'total' => $total,
+			'payment' => $payment,
+			'form' => $form->createView(),
+		);
+	}
+
+	/**
+	 * @Route("/{id}/create", name="payment_create")
+	 * @Template("@Sales/payment/new.html.twig")
+	 * @Method("POST")
+	 */
+	public function createAction(Request $request, $id)
 	{
         $payment = new Payment();
 		$form = $this->createForm('SalesBundle\Form\PaymentType', $payment);
@@ -68,21 +111,4 @@ class PaymentController extends Controller
 			'form' => $form->createView(),
 		);
 	}
-
-	/**
-	 * @Route("/{id}", name="payment_show")
-	 * @Template("@Sales/payment/show.html.twig")
-	 * @Method("GET")
-	 * @Security("has_role('ROLE_USER')")
-	 */
-	public function showAction($id)
-	{
-		$em = $this->getDoctrine()->getManager();
-		$payment = $em->getRepository('SalesBundle:Payment')->find($id);
-
-		return array(
-			'payment' => $payment,
-		);
-	}
-
 }
